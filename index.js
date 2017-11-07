5db58adbb73c66780de07723c119a9844431f5da
Ryan Shur <ryanshur32@yahoo.com>
2017-11-07 20:42:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const app = "I don't do much."
+
+var kittens =  ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+function appendKitten(name){
+  var catArr = kittens.slice("")
+  catArr.push(name)
+return catArr
+}
